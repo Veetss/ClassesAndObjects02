86eb28d3b704e3b92f8623ed6d28ef28d33f5227
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:15:05 +0100

    Update Team.java

--- src/Team.java
+++ src/Team.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Team {
+public class Team {
+    public String teamName;
+
+    Programmer p1 = new Programmer();
+    Programmer p2 = new Programmer();
+
+    public void printTeamDetails() {
+        System.out.println(teamName);
+        System.out.println(p1.name + p1.programmingLanguage + p1.yearsOfExperience);
+        System.out.println(p2.name + p2.programmingLanguage + p2.yearsOfExperience);
+    }
 }
